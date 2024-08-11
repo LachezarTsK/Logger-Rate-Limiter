eacbff2197e85cdfef68cdbca314a5b121df5268
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-08-11 14:20:12 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,20 @@
+
+class Logger {
+
+    static #MIN_WAIT_TIME_BETWEEN_PRINTING = 10;
+    #messageToLastPrintingTime = new Map();
+
+    /** 
+     * @param {number} timestamp 
+     * @param {string} message
+     * @return {boolean}
+     */
+    shouldPrintMessage(timestamp, message) {
+        if (!this.#messageToLastPrintingTime.has(message) ||
+                this.#messageToLastPrintingTime.get(message) + Logger.#MIN_WAIT_TIME_BETWEEN_PRINTING <= timestamp) {
+            this.#messageToLastPrintingTime.set(message, timestamp);
+            return true;
+        }
+        return false;
+    }
+}
